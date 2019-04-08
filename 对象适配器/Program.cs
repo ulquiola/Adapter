5b94540969dcc9008f37d3c63521421819cfec18
一心 <ulquiola@163.com>
2019-04-08 09:30:07 +0800

    update

--- 对象适配器/Program.cs
+++ 对象适配器/Program.cs
@@ -7,48 +7,58 @@ using System.Threading.Tasks;
 namespace 对象适配器
 {
     /// <summary>
-    /// 入口
+    /// 客户希望的类
+    /// 输出电压
+    /// 5V
     /// </summary>
-    class Portal
+    class Target
     {
-        private readonly string _msg;
-
-        public Portal(string msg)
+        public virtual void Output()
         {
-            _msg = msg;
-        }
-
-        public void Input()
-        {
-            Console.WriteLine(_msg + " --> 18v");
-        }
-    }
-    class Adapter : Export
-    {
-        private readonly Portal _portal;
-
-        public Adapter(string msg)
-        {
-            _portal = new Portal(msg);
-        }
-
-        public override void Output()
-        {
-            _portal.Input();
+            Console.WriteLine("输出电压-----5V");
         }
     }
     /// <summary>
-    /// 出口
+    /// 已经拥有的类
+    /// 需要适配的类
     /// </summary>
-    abstract class Export
+    class Adaptee
     {
-        public abstract void Output();
+        public void Input()
+        {
+            Console.WriteLine("输入电压-----220V");
+        }
+
     }
+    /// <summary>
+    /// 适配器
+    /// 将电压220V转化为5V
+    /// </summary>
+    class Adapter:Target
+    {
+        private Adaptee adaptee = new Adaptee();//建立私有Adaptee对象
+        /// <summary>
+        /// 覆写方法
+        /// 将220V输入转化为5V输出
+        /// 适配器提供5V的输出，但本质还是调用220V的输入方法
+        /// </summary>
+        public override void Output()
+        {
+            adaptee.Input();
+        }
+    }
+    /// <summary>
+    /// 客户端
+    /// 客户期待---手机通过充电头充电
+    /// 期待结果----输出电压为5V
+    /// </summary>
     class Client
     {
         static void Main(string[] args)
         {
-            
+            Target target = new Target();
+            target.Output();
+            Console.Read();
         }
     }
 }
